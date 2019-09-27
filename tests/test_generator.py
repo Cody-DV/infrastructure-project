6f3a69d1e27670b44386fac13f2bf906136cfb06
Cody DV <devilliers.cody@gmail.com>
2019-09-27 18:57:07 -0400

    Added flask - minor refactoring

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,20 +1,20 @@
 import unittest
 
-from app import app
+from generator import generator
 
 def test_sample_single_word():
     l = ('foo', 'bar', 'foobar')
-    word = app.sample(l)
+    word = generator.sample(l)
     assert word in l
 
 def test_sample_multiple_words():
     l = ('foo', 'bar', 'foobar')
-    words = app.sample(l, 2)
+    words = generator.sample(l, 2)
     assert len(words) == 2
     assert words[0] in l
     assert words[1] in l
     assert words[0] is not words[1]
 
 def test_generate_buzz_of_at_least_five_words():
-    phrase = app.generate()
+    phrase = generator.generate()
     assert len(phrase.split()) >= 5
