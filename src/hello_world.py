1003dcd3a57d70f842521e8b38354ef66fbac6f1
Cody DV <devilliers.cody@gmail.com>
2020-01-22 00:35:17 -0500

    Add greet endpoint to Flask API

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/greet', methods=['POST'])
+def greet_user():
+
+  data = json.loads(request.data)
+
+  response = jsonify({
+    "message": f"Greetings from the Flask API, {data['name']}", 
+    "status": "Online"
+  })
+
+  response.status_code = 200
+
+  return response
