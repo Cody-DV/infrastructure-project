9fafa40706cc51e08fde772de426e153e9b41063
Cody DV <devilliers.cody@gmail.com>
2020-01-25 12:03:01 -0500

    Add cors

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,13 +1,23 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
 
-@app.route('/greet', methods=['POST'])
+    response = jsonify({
+      "message": "Hello World"
+    })
+
+    response.status_code = 200
+
+    return response
+
+@app.route('/greet', methods=['POST', 'OPTIONS'])
 def greet_user():
 
   data = json.loads(request.data)
